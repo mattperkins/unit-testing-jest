bfc79975e657caab84ad23e5a0f92caad4eb7ea0
mattperkins <github@mattperkins.com>
2018-04-23 14:11:18 +0100

    init

---
+++ functions.js
@@ -0,0 +1,9 @@
+const functions = {
+    add: (num1, num2) => num1 + num2
+    // long hand version of above
+    // add: function(num1, num2) {
+    //     return num1 + num2
+    // }
+}
+
+export default functions;
---
+++ functions.test.js
@@ -0,0 +1,7 @@
+import functions from './functions'
+// const functions = require('./functions')
+
+test('Add 2 + 2 t0 equal 4', () => {
+    expect(functions.add(2,2)).toBe(4)
+})
+
